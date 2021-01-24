9cfc84184c62032a34ab138c01c23264d2678751
Samuel Folledo <samuelfolledo@gmail.com>
2021-01-23 23:56:15 -0500

    [add] finish example 3

--- Debugging Activities/exercise-3.py
+++ Debugging Activities/exercise-3.py
@@ -12,7 +12,7 @@ Exercise 3
 # - What line number is causing the error?
 #   - while key < arr[j]
 # - What can you deduce about the cause of the error?
-#   - Developer does not know how to insert error 
+#   - Developer does not know how to insert error and to worry about the last element to move
 
 
 # PART 2: State Assumptions
@@ -20,6 +20,7 @@ Exercise 3
 # TODO: State your assumptions here or say them out loud to your partner ...
 # Make sure to be SPECIFIC about what each of your assumptions is!
 # HINT: It may help to draw a picture to clarify what your assumptions are.
+#   - Developer didnt foresee that if element to move (j's index) is less than the length of arr, then that number will not exist anymore
 
 # What is insertion sort: https://www.youtube.com/watch?v=JU767SDMDvA
 
@@ -27,16 +28,11 @@ def insertion_sort(arr):
     """Performs an Insertion Sort on the array arr."""
     for i in range(1, len(arr)-1):
         key = arr[i] 
-
         j = i-1
-        print(key, j)
-        while key < arr[j] :
-            print(key, " is less than ", arr[j])
+        while key < arr[j] and j >= 0: #Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
             arr[j+1] = arr[j] 
             j -= 1
-            print("arr =", arr)
         arr[j+1] = key
-        print("Now ", arr)
     return arr
 
 if __name__ == '__main__':
