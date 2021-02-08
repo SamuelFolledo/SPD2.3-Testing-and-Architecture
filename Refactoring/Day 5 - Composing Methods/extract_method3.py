5fb92ccddeaefd8f11c157c25e33d81302210d8f
Samuel Folledo <samuelfolledo@gmail.com>
2021-02-07 23:33:27 -0500

    [add] solution for distance of two circles with doc strings

--- Refactoring/Day 5 - Composing Methods/extract_method3.py
+++ Refactoring/Day 5 - Composing Methods/extract_method3.py
@@ -1,14 +1,23 @@
 # Written by Kamran Bigdely
 # Example for Compose Methods: Extract Method.
 import math
+
 xc1 = 4
 yc1 = 4.25
 
 xc2 = 53
 yc2 = -5.35
 # Calculate the distance between the two circle
-distance = math.sqrt((xc1-xc2)**2 + (yc1 - yc2)**2)
+distance = distance_of_two_circles(xc1, yc1, xc2, yc2)
 print('distance', distance)
+
+def distance_of_two_circles(x_circle1, y_circle1, x_circle2, y_circle2):
+    """Calculate the distance between the two circle"""
+    x_distance = (x_circle1 - x_circle2)**2
+    y_distance = (y_circle1 - y_circle2)**2
+    distance = math.sqrt(x_distance + y_distance)
+    return distance
+
 # *** somewhere else in your program ***
 xa = -36
 ya = 97
