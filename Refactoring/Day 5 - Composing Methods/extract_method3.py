84980c0e59355af163442d519bf8eff464a05c11
Samuel Folledo <samuelfolledo@gmail.com>
2021-02-07 23:40:19 -0500

    [add] completed extra methods3

--- Refactoring/Day 5 - Composing Methods/extract_method3.py
+++ Refactoring/Day 5 - Composing Methods/extract_method3.py
@@ -7,9 +7,6 @@ yc1 = 4.25
 
 xc2 = 53
 yc2 = -5.35
-# Calculate the distance between the two circle
-distance = distance_of_two_circles(xc1, yc1, xc2, yc2)
-print('distance', distance)
 
 def distance_of_two_circles(x_circle1, y_circle1, x_circle2, y_circle2):
     """Calculate the distance between the two circle"""
@@ -18,12 +15,22 @@ def distance_of_two_circles(x_circle1, y_circle1, x_circle2, y_circle2):
     distance = math.sqrt(x_distance + y_distance)
     return distance
 
-# *** somewhere else in your program ***
+distance = distance_of_two_circles(xc1, yc1, xc2, yc2)
+print('distance', distance)
+
+
+################################################################################################
 xa = -36
 ya = 97
 
 xb = .34
 yb = .91
-# calcualte the length of vector AB vector which is a vector between A and B points.
-length = math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
+def length_of_vector(xa, xb, ya, yb):
+    """calculate the length of vector AB vector which is a vector between A and B points."""
+    x_length = (xa-xb)*(xa-xb)
+    y_length = (ya-yb)*(ya-yb)
+    length = math.sqrt(x_length + y_length)
+    return length
+
+length = length_of_vector(xa, xb, ya, yb)
 print('length', length)
