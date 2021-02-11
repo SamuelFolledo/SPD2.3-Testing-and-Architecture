699369686d28972099fdf62c14813dae2a5ed664
Samuel Folledo <samuelfolledo@gmail.com>
2021-02-11 17:35:11 -0500

    [add] cleaned and completed excercise 6

--- Refactoring/Day 5 - Composing Methods/6-replace_temp_with_query.py
+++ Refactoring/Day 5 - Composing Methods/6-replace_temp_with_query.py
@@ -20,30 +20,56 @@ class Student:
 class School:
     def __init__(self, students) -> None:
         self.students = students
+
     def process_graduation(self):
-        # Find the students in the school who have successfully graduated.
+        """
+            Find, print, congratulate the school's graduates.
+            Lastly, recommend the top graduates to top employers
+        """
+        self.find_graduates()
+        self.print_graduates()
+        self.congratulate_graduates()
+        self.recommend_top_students_to_top_employers()
+
+    def find_graduates(self):
+        """Find the students in the school who have successfully graduated."""
         min_gpa = 2.5 # minimum acceptable GPA
         passed_students = []
-        for s in self.students:
-            if s.get_gpa() > min_gpa:
-                passed_students.append(s)
+        for student in self.students:
+            if student.get_gpa() > min_gpa:
+                passed_students.append(student)
+        self.passed_students = passed_students
 
-        # print student's name who graduated.
+    def print_graduates(self):
+        """Print students who graduated."""
         print('*** Student who graduated *** ')
-        for s in passed_students:
-            print(s.name)
+        for student in self.passed_students:
+            print(student.name)
         print('****************************')
-        # Send congrat emails to them.
-        for s in passed_students:
-            s.send_congrat_email()
-        # Find the top 10% of them and send their contact to the top employers
-        passed_students.sort(key=lambda s: s.get_gpa())
+
+    def congratulate_graduates(self):
+        """Send congrat emails to them."""
+        for student in self.passed_students:
+            student.send_congrat_email()
+
+    def recommend_top_students_to_top_employers(self):
+        """Get the top students to recommend to top employers."""
         percentile = 0.9
-        index = int(percentile * len(passed_students))
-        top_10_percent = passed_students[index:]
+        top_10_percent = self.get_top_graduates(percentile)
         top_employers = [Employer('Microsoft'), Employer('Free Software Foundation'), Employer('Google')]
-        for e in top_employers:
-            e.send(top_10_percent)
+        self.recommend_graduates(top_10_percent, top_employers)
+
+    def get_top_graduates(self, percentile):
+        """Get top graduates based on the given percentile."""
+        sorted_graduates = self.passed_students.sort(key=lambda student: student.get_gpa())
+        index = int(percentile * len(sorted_graduates))
+        top_10_percent = sorted_graduates[index:]
+        return top_10_percent
+
+    def recommend_graduates(self, graduates, employers):
+        """Recommends graduates to employers."""
+        for employer in employers:
+            employer.send(graduates)
 
 students = [Student(2.1, 'donald'), Student(2.3, 'william'), Student(2.7, 'toro'), 
             Student(3.9, 'lili'), Student(3.2,'kami'), Student(3,'sarah')]
