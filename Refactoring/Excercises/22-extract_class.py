60e3a5c46c54d2cf3859de0666d91a89c42bbff5
Samuel Folledo <samuelfolledo@gmail.com>
2021-02-19 00:15:47 -0500

    [add] intiialized and food and added a print details. completed

--- Refactoring/Excercises/22-extract_class.py
+++ Refactoring/Excercises/22-extract_class.py
@@ -26,34 +26,30 @@ class Food:
         self.cuisine = cuisine
         self.ingredients = ingredients
         self.recipe = recipe
+    
+    def print_details(self):
+        print("Name:", name)
+        print("Prep time:", prep_time, "mins")
+        print("Is Veggie?", is_veggie)
+        print("Food Type:", food_type)
+        print("Cuisine:", cuisine)
+        for ingredient in self.ingredients:
+            print(ingredient, end=', ')
+        print()
+        print("recipe", recipe)
+        print("***")
 
 
 for key, value in foods.items():
     name = key
     prep_time = value[0]
     is_veggie = 'Yes' if value[1] else "No"
-    
     food_type = value[2]
     cuisine = value[3]
-
     ingredients = []
     for ingredient in value[4]:
         ingredients.append(ingredient)
-        # print(item, end=', ')
     recipe = value[5]
 
     food = Food(name, prep_time, is_veggie, food_type, cuisine, ingredients, recipe)
-    print(food.name)
-    # print("Name:",key)
-    # print("Prep time:",value[0], "mins")
-    # print("Is Veggie?", 'Yes' if value[1] else "No")
-    # print("Food Type:", value[2])
-    # print("Cuisine:", value[3])
-    # for item in value[4]:
-    #     print(item, end=', ')
-    # print()
-    # print("recipe", value[5])
-    # print("***")
-
-
-
+    food.print_details()
