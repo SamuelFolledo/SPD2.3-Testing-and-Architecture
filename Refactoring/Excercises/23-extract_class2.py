21e4ac411777877579199dae103c5291ed29eec1
Samuel Folledo <samuelfolledo@gmail.com>
2021-02-19 22:29:57 -0500

    [add] completed excercise 23

--- Refactoring/Excercises/23-extract_class2.py
+++ Refactoring/Excercises/23-extract_class2.py
@@ -1,20 +1,44 @@
 # by Kami Bigdely
 # Extract class
+
+class User:
+    def __init__(self, first_name, last_name, birth_year, movies, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_year = birth_year    
+        self.movies = movies
+        self.email = email
+
+    def send_hiring_email(self):
+        print("email sent to: ", self.email) 
+
+    def print_movies(self):
+        for movie in self.movies:
+            print(movie, end=', ')
+    
+    def custom_method(self):
+        """Custom method that can be customized for anything."""
+        print(self.first_name, " ", self.last_name)
+        self.print_movies()
+        self.send_hiring_email()
+        print()
+
 first_names = ['elizabeth', 'Jim']
 last_names = ['debicki', 'Carrey']
-birth_year = [1990, 1962]
+birth_years = [1990, 1962]
 movies = [['Tenet', 'Vita & Virgina', 'Guardians of the Galexy', 'The Great Gatsby'],\
-     ['Ace Ventura', 'The Mask', 'Dubm and Dumber', 'The Truman Show', 'Yes Man']]
+     ['Ace Ventura', 'The Mask', 'Dumb and Dumber', 'The Truman Show', 'Yes Man']]
 emails = ['deb@makeschool.com', 'jim@makeschool.com']
 
-def send_hiring_email(email):
-    print("email sent to: ", email)
+users = []
     
-for i, value in enumerate(emails):
-    if birth_year[i] > 1985:
-        print(first_names[i], last_names[i])
-        print('Movies Played: ', end='')
-        for m in movies[i]:
-            print(m, end=', ')
-        print()
-        send_hiring_email(value)
+for i, _ in enumerate(emails):
+    first_name = first_names[i]
+    last_name = last_names[i]
+    birth_year = birth_years[i]
+    movie_list = movies[i]
+    email = emails[i]
+    user = User(first_name, last_name, birth_year, movie_list, email)
+    users.append(user)
+    if user.birth_year > 1985:
+        user.custom_method()
