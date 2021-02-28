0adfa4dc0900f753f31d13b552f1a649325093b8
Samuel Folledo <samuelfolledo@gmail.com>
2021-02-28 03:53:23 -0500

    [add] compelted excercise 4

--- pytest/pytest-tut/excercise4.py
+++ pytest/pytest-tut/excercise4.py
@@ -10,8 +10,37 @@ def extract_position(line):
     pos = line[start_index:] # from start_index to the end.
     return pos
 
-if __name__ == "__main__":
-    result1 = extract_position('|error| numerical calculations could not converge.')
-    print(result1)
-    result2 = extract_position('|update| the positron location in the particle accelerator is x:21.432')
-    print(result2)
+# if __name__ == "__main__":
+#     result1 = extract_position('|error| numerical calculations could not converge.')
+#     print(result1)
+#     result2 = extract_position('|update| the positron location in the particle accelerator is x:21.432')
+#     print(result2)
+
+
+########## TESTS ##########
+
+# FIXTURES
+@pytest.fixture
+def average_input():
+    return "|update| the positron location in the particle accelerator is x:21.432"
+
+@pytest.fixture
+def null_input():
+    return None
+
+@pytest.fixture
+def error_input():
+    return "|error| numerical calculations could not converge."
+
+# TESTS
+def test_extract_position_average_case(average_input):
+    # Expected Input - contains 'x:
+    assert extract_position(average_input) == "21.432"
+
+def test_extract_position_null_case(null_input):
+    # NoneType Input
+    assert extract_position(null_input) == None
+
+def test_extract_position_error_case(error_input):
+    # Error Input - 'debug' or 'error'
+    assert extract_position(error_input) == None
